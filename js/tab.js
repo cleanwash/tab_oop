d6f2068818580dc674b7e7e1b656e00b783be695
HIL <kenbugless@gmail.com>
2023-11-11 11:40:53 +0900

    탭메뉴 기본 기능 구현

--- js/tab.js
+++ js/tab.js
@@ -1,17 +1,21 @@
-class Student {
-	//일일 변경될 필요가 없는 공통의 값은 인스턴스 객체가 아니라 class 자체적으로 등록해서 전달가능 (멤버변수)
-	//public : 외부에서 변경가능한 멤버변수
-	//school = 'dcodelab';
-	//static : 외부에서 변경불가능한 멤버변수
-	//private : 외부에서 변경불가능하게 처리
-	#school = 'dcodelab';
+var tab = document.querySelector('#tab1');
+var btns = tab.querySelectorAll('ul li');
+var boxs = tab.querySelectorAll('section article');
 
-	constructor(name, age, gender) {
-		this.name = name;
-		this.age = age;
-		this.gender = gender;
-	}
+btns.forEach(function (btn, idx) {
+	btn.addEventListener('click', function () {
+		//activation(btns, idx);
+		//activation(boxs, idx);
+		[btns, boxs].forEach(function (el) {
+			activation(el, idx);
+		});
+	});
+});
+
+function activation(arr, idx) {
+	arr.forEach(function (el) {
+		el.classList.remove('on');
+	});
+
+	arr[idx].classList.add('on');
 }
-
-const s1 = new Student('David', 20, 'male');
-const s2 = new Student('Emily', 22, 'female');
