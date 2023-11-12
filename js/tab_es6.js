e05d9de37162a4400b53a9e2d99c4a32f0478478
HIL <kenbugless@gmail.com>
2023-11-12 09:28:55 +0900

    transition tab에 fade기능 추가

--- js/tab_es6.js
+++ js/tab_es6.js
@@ -1,24 +1,3 @@
-/*
-const tab = document.querySelector('#tab1');
-const btns = tab.querySelectorAll('ul li');
-const boxs = tab.querySelectorAll('section article');
-
-btns.forEach((btn, idx) => {
-	btn.addEventListener('click', () => {
-		[btns, boxs].forEach((el) => {
-			activation(el, idx);
-		});
-	});
-});
-
-function activation(arr, idx) {
-	arr.forEach((el) => {
-		el.classList.remove('on');
-	});
-
-	arr[idx].classList.add('on');
-}
-*/
 class Tab {
 	constructor(el) {
 		this.init(el);
