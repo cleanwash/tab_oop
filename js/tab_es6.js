76dc6af1c5f4f3493c61ce82de0794cd33ac619b
HIL <kenbugless@gmail.com>
2023-11-12 11:03:11 +0900

    transition설정

--- js/tab_es6.js
+++ js/tab_es6.js
@@ -16,6 +16,10 @@ class Tab {
 		this.boxs = this.el.querySelectorAll('section article');
 		this.setHeight(0);
 
+		//frame의 높이값에 transition속성 옵션 speed값에 따로 동적 적용
+		this.el.style.transitionProperty = 'height';
+		this.el.style.transitionDuration = this.fadeSpeed / 1000 + 's';
+
 		this.boxs.forEach((el) => {
 			el.style.transitionProperty = 'opacity';
 			el.style.transitionDuration = this.fadeSpeed / 1000 + 's';
@@ -42,10 +46,9 @@ class Tab {
 			el.classList.remove(this.activeClass);
 		});
 
-		arr[idx].classList.add(this.activeClass);
 		this.setHeight(idx);
+		arr[idx].classList.add(this.activeClass);
 
-		//activeClass붙어서 활성화 되고 fadeSpeed만큼 트랜지션 모션이 끝난이후에 다시 eventBlock값을 false로 바꿔서 이벤트 연결가능처리
 		setTimeout(() => (this.eventBlock = false), this.fadeSpeed);
 	}
 
