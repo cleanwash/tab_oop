20020fd20a11f62c000613f20eda23bc0cd8b2f9
HIL <kenbugless@gmail.com>
2023-11-12 10:16:19 +0900

    활성화버튼 재이벤트 방지, 모션중 재이벤트 방지

--- js/tab_es6.js
+++ js/tab_es6.js
@@ -5,6 +5,7 @@ class Tab {
 		const resultOpt = { ...defOpt, ...opt };
 		this.activeClass = resultOpt.activeClass;
 		this.fadeSpeed = resultOpt.fadeSpeed;
+		this.eventBlock = false;
 
 		this.init(el);
 		this.bindingEvent();
@@ -21,7 +22,11 @@ class Tab {
 
 	bindingEvent() {
 		this.btns.forEach((btn, idx) => {
-			btn.addEventListener('click', () => {
+			btn.addEventListener('click', (e) => {
+				//현재 클릭한 버튼 요소에 활성화 클래스명이 있거나 this.eventBlock이 true면 return으로 함수호출 중지
+				if (e.currentTarget.classList.contains(this.activeClass) || this.eventBlock) return;
+				this.eventBlock = true;
+
 				[this.btns, this.boxs].forEach((el) => {
 					this.activation(el, idx);
 				});
@@ -30,10 +35,14 @@ class Tab {
 	}
 
 	activation(arr, idx) {
+		console.log('activation');
 		arr.forEach((el) => {
 			el.classList.remove(this.activeClass);
 		});
 
 		arr[idx].classList.add(this.activeClass);
+
+		//activeClass붙어서 활성화 되고 fadeSpeed만큼 트랜지션 모션이 끝난이후에 다시 eventBlock값을 false로 바꿔서 이벤트 연결가능처리
+		setTimeout(() => (this.eventBlock = false), this.fadeSpeed);
 	}
 }
